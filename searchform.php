b7d924efc0ccbfbc2e782b6cdb6059bec2b0ae93
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-07 14:33:07 +0900

    虫眼鏡icon画像　絶対パスへ変更

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form class="p-search-form" id="searchform" method="get" action="<?php echo home_url('/'); ?>">
             <input   placeholder=""  class="p-search-form__input" type="search" name="s" id="s">
-            <a class="js-search-form__icon"><img class="p-search-form__icon" src="img/search-icon.png"></a>
+            <a class="js-search-form__icon"><img class="p-search-form__icon" src="<?php echo esc_url(get_theme_file_uri('/img/search-icon.png'));?>"></a>
             <button class= "p-search-form__button" ><a class= "p-search-form__button__label">検索</a></button>
           </form>
