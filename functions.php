1f711f660c00849d6b8fd5b1629e9289c3dd3275
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-08 15:25:15 +0900

    function.php作成

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+    //テーマサポート
+    add_theme_support( 'menus' );
+    add_theme_support( 'title-tag' );
+
+    //タイトル出力
+    function WordPress__Hamburger_title( $title ) {
+        if ( is_front_page() && is_home() ) { //トップページなら
+            $title = get_bloginfo( 'name', 'display' );
+        } elseif ( is_singular() ) { //シングルページなら
+            $title = single_post_title( '', false );
+        }
+            return $title;
+        }
+    add_filter( 'pre_get_document_title', 'WordPress__Hamburger_title' );
+
+    function add_files() {
+        wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+  }
+  add_action( 'wp_enqueue_scripts', 'add_files' );
--- header.php
+++ header.php
@@ -4,11 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=375,initial-scale=1">
-  <title>
-    <?php
-    wp_title('|,true,right');
-    bloginfo('name'); ?></title>
-  <title>Document</title>
+
   <link rel="stylesheet" href="<?php echo esc_url(get_theme_file_uri('/Scss/common.css'));?>" type="text/css">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-rqn26AG5Pj86AF4SO72RK5fyefcQ/x32DNQfChxWvbXIyXFePlEktwD18fEz+kQU" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500;700&display=swap" rel="stylesheet">
@@ -18,14 +14,14 @@
   <script src="<?php echo esc_url(get_theme_file_uri('/jquery/script_search-form.js'));?>"></script>
   <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <div class="l-body js-body">
     <header  class="l-header">
       <div class="p-header">
         <div class="p-header__wrapper">
           <div class="c-wrapper__logo">
-            <h1 class="c-logo--title--hamburger">Hamburger</h1>
+            <a href="<?php echo esc_url(home_url());?>"><h1 class="c-logo--title--hamburger">Hamburger</h1></a>
           </div>
           <?php get_search_form(); ?>
       <div class="c-btn--menu__wrapper">
