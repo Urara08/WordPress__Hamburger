1aaa2583879ee8249343465d57912cdcab42e7cd
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-09 17:23:47 +0900

    sidebarをnav.menu化

---
+++ sample.php
@@ -0,0 +1,30 @@
+
+      <li><strong class="p-sidebar__nav__title">バーガー</strong>
+          <ul class="p-sidebar__nav__list">
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">ハンバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">チーズバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">テリヤキバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">アボガドバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">フィッシュバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">ベーコンバーガー</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">チキンバーガー</a></li>
+          </ul>
+        </li>
+        <li><strong class="p-sidebar__nav__title">サイド</strong>
+          <ul class="p-sidebar__nav__list">
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">ポテト</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">サラダ</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">ナゲット</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">コーン</a></li>
+          </ul>
+        </li>
+        <li><strong class="p-sidebar__nav__title">ドリンク</strong>
+          <ul class="p-sidebar__nav__list">
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">コーラ</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">ファンタ</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">オレンジ</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">アップル</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">紅茶（Ice/Hot)</a></li>
+            <li class="p-sidebar__nav__list__item"><a href="#" class="c-item__title">コーヒー（Ice/Hot)</a></li>
+          </ul>
+        </li>
