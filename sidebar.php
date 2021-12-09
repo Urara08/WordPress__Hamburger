2e1068d50c5d820f6f7cf4e2680a22cd58893db9
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-09 20:54:00 +0900

    sidebarカスタムメニューへ変更

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,14 @@
         <i class=" fal fa-times fa-3x js-sidebar__menu__close-btn"></i>
       </a>
       <ul class="p-sidebar__nav"><a class="p-sidebar__nav__label">Menu</a>
-      <?php wp_nav_menu(); ?>
-      </ul>
+      <li><strong class="p-sidebar__nav__title">バーガー</strong>
+      <?php wp_nav_menu( array('theme_location' => 'item_1',) ); ?>
+      </li>
+      <li><strong class="p-sidebar__nav__title">サイド</strong>
+      <?php wp_nav_menu( array('theme_location' => 'item_2',) ); ?>
+      </li>
+      <li><strong class="p-sidebar__nav__title">ドリンク</strong>
+      <?php wp_nav_menu( array('theme_location' => 'item_3',) ); ?>
+    </ul>
     </div>
   </aside>
