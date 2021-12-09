ae6c269923bf60af411fe43da663767f2e528089
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-09 17:15:31 +0900

    footer　メニュー機能外す

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
+
 <footer class="l-footer">
         <div class="p-footer">
           <div class="p-footer__wrapper">
             <ul class="p-footer__menu">
-              <?php wp_nav_menu(); ?>
-            　<li class="p-footer__menu__copy">Copyright<? echo date('Y')?>: RaiseTech</li>
+              <li class="p-footer__menu__info">ショップ情報 | ヒストリー</li>
+            　<li class="p-footer__menu__copy">Copyright: RaiseTech</li>
           </ul>
         </div>
       </div>
