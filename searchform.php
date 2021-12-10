5ee5bfc5890babd9daec7b76863289c7b8d0e15f
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-10 23:44:00 +0900

    search修正

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <form class="p-search-form" id="searchform" method="get" action="<?php echo home_url('/'); ?>">
-            <input   placeholder=""  class="p-search-form__input" type="search" name="s" id="s">
+            <input class="p-search-form__keyword" placeholder="" name="s" id="s">
             <a class="js-search-form__icon"><img class="p-search-form__icon" src="<?php echo esc_url(get_theme_file_uri('/img/search-icon.png'));?>"></a>
+            <input class="p-search-form__submit" id="searchsubmit" type="submit" value="検索">
             <button class= "p-search-form__button" ><a class= "p-search-form__button__label">検索</a></button>
           </form>
