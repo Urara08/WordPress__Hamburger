a29c0e7bffd48db095a0c27ac2f15008c42efb25
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-12 17:52:12 +0900

    components/archive作成中

---
+++ components/archive.php
@@ -0,0 +1,29 @@
+<article class="p-contents__archive">
+<div class="p-contents__archive__inner--top">
+  <section class="p-contents__archive__introduction"><!-- 導入記事　ここから -->
+  <div class="p-contents__archive__introduction__inner">
+    <h2 class="p-contents__archive__introduction__title">小見出しが入ります</h2>
+    <p class="p-contents__archive__introduction__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+  </div>
+</section><!-- 導入記事　ここまで -->
+</div><!-- p-contents__archive__inner--top　ここまで -->
+<div class="p-contents__archive__inner--middle">
+  <section class="p-contents__archive__cards"><!-- card　ここから -->
+  <div class="c-wrapper--card--archive">
+    <figure class="c-card--archive"><!-- 商品詳細　① -->
+    <div class="c-photo-frame--card--archive">
+      <div class="c-photo--archive" alt="ハンバーガーの画像"></div>
+              </div>
+              <figcaption class="c-caption-group--card--archive">
+                <h3 class="c-category--card">チーズバーガー</h3>
+                <h4 class="c-sub-heading--card">小見出しが入ります</h4>
+                <p class="c-detail--card--archive">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                <div class="c-btn-wrapper--card--archive">
+                  <div class="c-btn--card"><a class="c-btn-label--card">詳しくみる</a></div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+          </article>
+        <?php get_footer(); ?>
+        <?php get_sidebar(); ?>
