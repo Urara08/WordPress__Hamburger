77168c7301279564d4c672da2ee855df67b2f9aa
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 18:33:33 +0900

    photo-add-text ブロックエディタ追加

---
+++ blocks/block-photo-add-text.php
@@ -0,0 +1,12 @@
+<!-- 'photo-add-sentence1'　フラッシュ画像写真（文章あり）－１-->
+
+<div class="c-wrapper--card--single hp_mb117">
+  <figure class="c-card--single--special">
+    <div class="c-photo-frame--card--single--various">
+      <div class="c-photo--card--single--various" style="background-image: url(<?php block_field( 'photo-add-text-photo' ); ?>)"></div>
+    </div>
+    <figcaption class="c-caption-group--card--single">
+      <p class="c-detail--card--single"><?php block_field( 'photo-add-text-text' ); ?></p>
+    </figcaption>
+  </figure>
+</div>
