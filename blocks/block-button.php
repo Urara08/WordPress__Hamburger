df8bc593432d45475386f14c031b25eb7a194598
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 21:43:46 +0900

    ボタン　ブロックエディタ制作

---
+++ blocks/block-button.php
@@ -0,0 +1,9 @@
+<!-- 'button'　ボタン-->
+
+<?php block_field('table-title');?></p></th><td class="p-contents__single__block-table__item"><p><?php block_field('table-detail');?>
+<div class="p-contents__single__block__area">
+      <div class="p-contents__single__block-button__wrapper">
+        <a class="p-contents__single__block-button" href="<?php block_field('button-link');?>"><p class="c-btn-label"><?php block_field('buttontext');?></p></a>
+        <p class="p-contents__single__block-button__supplement"><?php block_field('button-description');?></p>
+      </div>
+    </div>
