42e76deef8e2fdfa18828252fc17621ce004728f
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 17:09:08 +0900

    ブロックエディタ（引用・写真フル追加）

---
+++ blocks/block-album.php
@@ -0,0 +1,9 @@
+<!-- アルバム（画像）のブロック -->
+<!-- 'album-full'（横一枚） -->
+<div class="p-contents__single__block__area editor">
+<figure class="c-card--single">
+  <div class="c-photo-frame--card--single--no-padding">
+    <div class="c-photo--single" style="background-image: url(<?php block_field( 'album-full' ); ?>)"></div>
+  </div>
+</figure>
+</div>
---
+++ blocks/block-quote.php
@@ -0,0 +1,9 @@
+<!-- 引用タグのブロック -->
+<div class="p-contents__single__block__area editor">
+<div class="quote p-contents__single__block-quote">
+<div class="p-contents__single__block-quote__inner">
+<p class="p-contents__single__block-quote__text"><?php block_field( 'quote-description' ); ?></p>
+<p class="p-contents__single__block-quote__source"><h3 class="p-contents__single__block-quote__source__title"><?php block_field( 'quote-source' ); ?>:</h3><a class="p-contents__single__block-quote__source__link"><?php block_field( 'quote-source-link' )  ; ?></a></p>
+</div>
+</div>
+</div>
