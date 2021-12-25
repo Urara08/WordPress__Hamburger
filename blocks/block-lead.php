0ae2f9335ed839904c66196077afcc07635f5018
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 22:19:09 +0900

    リード記事のブロックエディタ作成

---
+++ blocks/block-lead.php
@@ -0,0 +1,10 @@
+<!-- 'lead'　リード-->
+
+<div class="p-contents__single__block-lead__area">
+  <section class="p-contents__single__block-lead">
+    <div class="p-contents__single__block-lead__inner">
+      <h2 class="p-contents__single__block-lead__title  tag-h2"><?php block_field( 'lead-title' ); ?></h2>
+      <p class="p-contents__single__block-lead__text"><?php block_field( 'lead-text' ); ?></p>
+    </div>
+  </section>
+</div>
