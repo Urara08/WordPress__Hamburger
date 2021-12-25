3f9c2df12f5c6bb94a0f44d8d54fce6ebe656447
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 22:34:37 +0900

    ブロックエディタ　３番目のcard余白修正（文章＋画像）

--- blocks/block-text-add-photo.php
+++ blocks/block-text-add-photo.php
@@ -1,9 +1,9 @@
 <!-- 'text-add-photo '　文章（画像有）※文章が左・画像が右-->
 
 <div class="c-wrapper--card--single">
-  <figure class="c-card--single--special">
-    <figcaption class="c-caption-group--card--single --for-pc">
-      <p class="c-detail--card--single"><?php block_field( 'text-add-photo-text' ); ?></p>
+              <figure class="c-card--single--special">
+                <figcaption class="c-caption-group--card--single --for-pc">
+                  <p class="c-detail--card--single"> <?php block_field( 'text-add-photo-text' ); ?></p>
     </figcaption>
 
     <div class="c-photo-frame--card--single--various">
