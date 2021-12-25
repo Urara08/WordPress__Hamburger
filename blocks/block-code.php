d3bf19cb0eac60ec909947048718b89fd1fe661d
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 21:01:42 +0900

    コードブロック作成

---
+++ blocks/block-code.php
@@ -0,0 +1,7 @@
+<!-- 'code'　コードブロック-->
+
+<div class="p-contents__single__block-code__wrapper">
+  <ul class="p-contents__single__block-code">
+    <li class="p-contents__single__block-code__detail"><p><?php block_field('code-text');?></p></li>
+  </ul>
+</div>
