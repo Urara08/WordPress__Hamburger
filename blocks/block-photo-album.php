e78585df41947686b3bfa3b53e24f3e441fd36eb
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 18:07:14 +0900

    ブロック（連続写真）追加

---
+++ blocks/block-photo-album.php
@@ -0,0 +1,48 @@
+<!-- 'photo-album'　フラッシュ画像 -->
+<div class="c-wrapper--card--single--continuous-area">
+              <div class="c-wrapper--card--single--continuous no_one">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_two">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_three">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous" style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_four">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_five">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_six">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_seven">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_eight">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+              <div class="c-wrapper--card--single--continuous no_nine">
+                <figure class="c-photo-frame--card--single--type_continuous">
+                  <div class="c-photo--single--continuous"  style="background-image: url(<?php block_field( 'photo-album' ); ?>)"></div>
+                </figure>
+              </div>
+            </div>
