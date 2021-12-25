03b2fea61292754421689b5debe568c550e51c7b
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-26 00:32:40 +0900

    single済み

--- single.php
+++ single.php
@@ -1,173 +1,40 @@
 <?php get_header(); ?>
-    <main class="l-main">
-      <div class="l-hero">
-        <section class="p-hero__single">
-          <div class="p-hero__single__png--sp" alt="画像"></div>
-          <div class="p-hero__single__png--pc" alt="画像"></div>
-          <h2 class="p-hero__label">h1 チーズバーガー</h2>
-        </section>
+<?php /*Template Name:アイテム;*/ ?>
+
+<main class="l-main">
+  <div class="l-hero">
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+
+      <section id="<?php the_title(); ?>" class="p-hero__single">
+      <div class="item_photo__wrapper">
+        <img class="item_photo" src="<?php echo CFS()->get('item_photo'); ?>" alt="<?php the_title(); ?>">
       </div>
-      <section class="p-contents__single">
-        <div class="p-contents__single__inner--top">
-          <div class="p-contents__single__block-lead__area">
-            <section class="p-contents__single__block-lead">
-              <div class="p-contents__single__block-lead__inner">
-                <h2 class="p-contents__single__block-lead__title  tag-h2">見出しH2</h2>
-                <p class="p-contents__single__block-lead__text">Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
-              </div>
-            </section>
-          </div>
-          <div class="p-contents__single__block__area">
-            <section class="p-contents__single__block-heading">
-              <div class="p-contents__single__block-heading__inner">
-                <h3 class="p-contents__single__block-heading__title  tag-h3">見出しH3</h3>
-              </div>
-            </section>
-          </div>
-          <div class="p-contents__single__block__area">
-            <section class="p-contents__single__block-heading">
-              <div class="p-contents__single__block-heading__inner">
-                <h4 class="p-contents__single__block-heading__title  tag-h4">見出しH4</h4>
-              </div>
-            </section>
-          </div>
-          <div class="p-contents__single__block__area">
-            <section class="p-contents__single__block-heading">
-              <div class="p-contents__single__block-heading__inner">
-                <h5 class="p-contents__single__block-heading__title  tag-h5">見出しH5</h5>
-              </div>
-            </section>
-          </div>
-          <div class="p-contents__single__block__area">
-            <section class="p-contents__single__block-heading">
-              <div class="p-contents__single__block-heading__inner">
-                <h6 class="p-contents__single__block-heading__title title tag-h6">見出しH6</h6>
-              </div>
-            </section>
-          </div>
-          <div class="p-contents__single__block__area">
-            <article class="p-contents__single__block-quote">
-              <div class="p-contents__single__block-quote__inner">
-                <div class="p-contents__single__block-quote__text"><p class="p-contents__single__block-quote__text--detail">引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引</p>
-                  <div class="p-contents__single__block-quote__source"><h3 class="p-contents__single__block-quote__source__title">出典元：</h3>　<a href="#" class="p-contents__single__block-quote__source__link">〇〇〇〇〇〇〇〇〇〇〇</a>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </div>
+      <h2 class="p-hero__label"><?php echo CFS()->get('item_title'); ?></h2>
+    </section>
+  </div>
+    <section class="p-contents__single">
+      <div class="p-contents__single__inner--top">
+        <div class="p-contents__single__block-lead__area">
+          <section class="p-contents__single__block-lead">
+            <div class="p-contents__single__block-lead__inner">
+              <h2 class="p-contents__single__block-lead__title  tag-h2"><?php echo CFS()->get('item_title'); ?></h2><br>
+              <p class="p-contents__single__block-lead__text"><?php echo CFS()->get('item_text'); ?></p>
+            </div>
+          </section>
         </div>
-        <div class="p-contents__single__inner--middle">
-          <section class="p-contents__single__cards">
-            <div class="c-wrapper--card--single hp_mb60">
-              <figure class="c-card--single">
-                <div class="c-photo-frame--card--single--no-padding">
-                  <div class="c-photo--single" alt="ハンバーガーの画像"></div>
-                </div>
-              </figure>
-            </div>
-            <div class="c-wrapper--card--single hp_mb117">
-              <figure class="c-card--single--special">
-                <div class="c-photo-frame--card--single--various">
-                  <div class="c-photo--card--single--various" alt="ハンバーガーの画像"></div>
-                </div>
-                <figcaption class="c-caption-group--card--single">
-                  <p class="c-detail--card--single">  テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
-                </figcaption>
-              </figure>
-            </div>
-            <div class="c-wrapper--card--single">
-              <figure class="c-card--single--special">
-                <figcaption class="c-caption-group--card--single --for-pc">
-                  <p class="c-detail--card--single">  テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
-                </figcaption>
-                <div class="c-photo-frame--card--single--various">
-                  <div class="c-photo--card--single--various" alt="ハンバーガーの画像"></div>
-                </div>
-              </figure>
-            </div>
-            <div class="c-wrapper--card--single--type_frame">
-              <figure class="c-photo-frame--card--single--type_frame">
-                <div class="c-photo--card--single--type_frame" alt="ハンバーガーの画像"></div>
-              </figure>
-            </div>
-            <div class="c-wrapper--card--single--pc-only">
-              <figure class="c-card--single--special pc-only">
-                <div class="c-photo-frame--card--single--various">
-                  <div class="c-photo--card--single--various" alt="ハンバーガーの画像"></div>
-                </div>
-              </figure>
-            </div>
-            <div class="p-contents__single__block__area"><div class="p-contents__single__block-list">
-              <div class="p-contents__single__block-list__inner">
-                <div class="p-contents__single__block-list">
-                  <ul class="p-contents__single__block-list-group--1">
-                    <li>1.リストリストリスト</li>
-                    <li>2.リストリストリスト</li>
-                  </ul>
-                  <ul class="p-contents__single__block-list-group--2">
-                    <li>1.リスト2リスト2リスト2</li>
-                    <li>2.リスト2リスト2リスト2</li>
-                  </ul>
-                  <ul class="p-contents__single__block-list-group--1 hp_mb60">
-                    <li>1.リストリストリスト</li>
-                    <li>2.リストリストリスト</li>
-                  </ul>
-                  <ul class="p-contents__single__block-list-group--1">
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト</li>
-                  </ul>
-                  <ul class="p-contents__single__block-list-group--2">
-                    <li>リスト2リスト2リスト2</li>
-                    <li>リスト2リスト2リスト2</li>
-                  </ul>
-                  <ul class="p-contents__single__block-list-group--1">
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="p-contents__single__block__area">
-            <div class="p-contents__single__block-code__wrapper">
-              <ul class="p-contents__single__block-code">
-                <li class="p-contents__single__block-code__detail">&lt;html&gt;</li>
-                <li class="p-contents__single__block-code__detail">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;head&gt;</li>
-                <li class="p-contents__single__block-code__detail">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/head&gt;</li>
-                <li class="p-contents__single__block-code__detail">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;body&gt;</li>
-                <li class="p-contents__single__block-code__detail">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/body&gt;</li>
-                <li class="p-contents__single__block-code__detail">&lt;/html&gt;</li>
-              </ul>
-            </div>
-            <div class="p-contents__single__block__area">
-              <table class="p-contents__single__block-table">
-                <div class="p-contents__single__table-block__inner">
-                  <tr>
-                    <th class="p-contents__single__block-table__title">テーブル</th><td class="p-contents__single__block-table__item">テーブル</td>
-                  </tr>
-                  <tr>
-                    <th class="p-contents__single__block-table__title">テーブル</th><td class="p-contents__single__block-table__item">テーブル</td>
-                  </tr>
-                  <tr>
-                    <th class="p-contents__single__block-table__title">テーブル</th><td class="p-contents__single__block-table__item">テーブル</td>
-                  </tr>
-                  <tr>
-                    <th class="p-contents__single__block-table__title">テーブル</th><td class="p-contents__single__block-table__item">テーブル</td>
-                  </tr>
-                </div>
-              </table>
-            </div>
-    <div class="p-contents__single__block__area">
-      <div class="p-contents__single__block-button__wrapper">
-        <a class="p-contents__single__block-button" href="#"><p class="c-btn-label">ボタン</p></a>
-        <p class="p-contents__single__block-button__supplement">boldboldboldboldboldboldbold</p>
-      </div>
-    </div>
-  </section>
-</div>
+        <?php endwhile;
+        else :
+      ?>
+      <p>表示する記事がありません</p>
+      <?php endif;
+      ?>
+
 <div class="p-contents__single__inner--bottom">
 </div>
+
 </section>
 </main>
+
 <?php get_footer(); ?>
-  <?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
