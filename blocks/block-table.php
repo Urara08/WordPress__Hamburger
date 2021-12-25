4c5c66fb8a17e9be51431df24c9e0867a9e44002
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-25 21:29:00 +0900

    ブロックエディタ　テーブル作成

---
+++ blocks/block-table.php
@@ -0,0 +1,8 @@
+<!-- 'table'　テーブル-->
+
+<div class="p-contents__single__block__area">
+  <table class="p-contents__single__block-table mt-0">
+    <tr>
+      <th class="p-contents__single__block-table__title"><p><?php block_field('table-title');?></p></th><td class="p-contents__single__block-table__item"><p><?php block_field('table-detail');?></p></td>
+    </tr>
+  </table>
--- single.php
+++ single.php
@@ -97,53 +97,7 @@
                 </div>
               </figure>
             </div>
-            <div class="c-wrapper--card--single--continuous-area">
-              <div class="c-wrapper--card--single--continuous no_one">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_two">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_three">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_four">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_five">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_six">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_seven">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_eight">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-              <div class="c-wrapper--card--single--continuous no_nine">
-                <figure class="c-photo-frame--card--single--type_continuous">
-                  <div class="c-photo--single--continuous" alt="ハンバーガーの画像"></div>
-                </figure>
-              </div>
-            </div>
+　
             <div class="p-contents__single__block__area"><div class="p-contents__single__block-list">
               <div class="p-contents__single__block-list__inner">
                 <div class="p-contents__single__block-list">
