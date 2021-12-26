76861509d7561543f07a790d51e37ac4df8e4f45
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-26 21:48:57 +0900

    pageのhero画像のバグ修正

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
+
 <main class="l-main">
-  <div class="p-hero"><!-- heroここから-->
-  <section class="p-hero__page">
-  <div class="p-hero__page__png--sp p-hero__page__png--pc " style="background-image: url(<?php the_post_thumbnail_url(); ?>)" alt="背景の画像"></div><!--アイキャッチ画像読み込み-->
+
+<section class="p-hero__page">
+  <div class="p-hero__page__png--sp p-hero__page__png--pc " style="background-image: url(<?php the_post_thumbnail_url(); ?>)" alt="背景の画像"></div><!-- アイキャッチ画像を表示-->
   <h2 class="p-hero__page__menu-label"><?php the_title(); ?></h2>
 </section>
-</div><!-- heroここまで-->
 
 <section class="p-contents__single"><!-- コンテンツここから->
   <div class="p-contents__single__inner--top"></div>
