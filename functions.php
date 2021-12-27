acb60a40ce059fde604278f074bf25e032473f76
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-12-28 02:21:20 +0900

    スマホの見た目修正

--- functions.php
+++ functions.php
@@ -18,14 +18,15 @@
 
     // CSS / JavaScriptの読み込み
     function add_files() {
+        wp_enqueue_style( 'stylesheet', get_theme_file_uri( '/Scss/common.css' ), false );//CSSの読み込み
         wp_enqueue_style( 'M+PLUS+1p', 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500;700&display=swap', array() );//googleフォント読み込み
         wp_enqueue_style( 'Roboto', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;700;900&display=swap', array() );//googleフォント読み込み
-        wp_enqueue_style( 'stylesheet', get_theme_file_uri( '/Scss/common.css' ), false );//CSSの読み込み
         wp_enqueue_style( 'font-awesome', 'https://pro.fontawesome.com/releases/v5.15.4/css/all.css', array(), '5.15.4' );//font_awesome読み込み
+
         wp_deregister_script( 'jquery'); //デフォルトの jQuery は読み込まない
-        wp_enqueue_script( 'jquery', get_template_directory_uri() . '/jquery/jquery-3.6.0.min.js', "", "3.6.0", true );// jQuery本体読み込み
-        wp_enqueue_script( 'sidebar.js', get_template_directory_uri() . '/jquery/script_sidebar.js', array('jquery'), '1.0.0', true);// jQueryスクリプトファイル読み込み
-        wp_enqueue_script( 'search-form.js', get_template_directory_uri() . '/jquery/script_search-form.js', array('jquery'), '1.0.0', true);// jQueryスクリプトファイル読み込み
+        wp_enqueue_script( 'jquery', get_template_directory_uri() . '/Jquery/jquery-3.6.0.min.js', "", "3.6.0", true );// jQuery本体読み込み
+        wp_enqueue_script( 'sidebar.js', get_template_directory_uri() . '/Jquery/script_sidebar.js', array('jquery'), '1.0.0', true);// jQueryスクリプトファイル読み込み
+        wp_enqueue_script( 'search-form.js', get_template_directory_uri() . '/Jquery/script_search-form.js', array('jquery'), '1.0.0', true);// jQueryスクリプトファイル読み込み
         }
         add_action( 'wp_enqueue_scripts', 'add_files' );
 
