5d7b6052c006cb0725f006d4fa30c344f2f30179
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-01-12 15:18:53 +0900

    虫眼鏡アイコンが消えるJS削除

--- Jquery/script_search-form.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('input').focusin(function(e)  {//検索ボックスがフォーカスされると
-      $('.js-search-form__icon').addClass('is-active')//虫眼鏡が消える
-    });
-
-  $('input').focusout(function(e)  {//検索ボックスのフォーカスが外れると
-      $('.js-search-form__icon').removeClass('is-active')//虫眼鏡が現れる
-    });
-  });
