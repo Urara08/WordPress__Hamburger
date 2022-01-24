fa59f5ae8e9db09e1e0ddd6e191ea5725e2f2599
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-01-24 16:10:04 +0900

    404ページ/スマホ/footerの浮き修正

--- 404.php
+++ 404.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
+
 <main class="l-main">
+<div class="p-contents__archive__inner--top"></div>
+
     <div class="container" style="margin:18px">
         <h1  style="margin-bottom:30px" >404 Page Not Found</h1>
         <p>お探しのページは見つかりませんでした。</p>
@@ -7,7 +10,8 @@
         <p>別のキーワードでお探し頂くか、</p>
         <p>申し訳ございませんが、<a style="font-size:18px"href="<?php echo esc_url( home_url()); ?>">こちらのリンク</a>からトップページにお戻りください。 </p>
     </div>
+
 </main><!-- main -->
-</main>
+
 <?php get_footer(); ?>
 <?php get_sidebar(); ?>
